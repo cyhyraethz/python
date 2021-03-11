8a27e5709297a29d9ac3a38593c9b89f73c12faa
Cyhyraeth <cyhyraethz@gmail.com>
2021-03-11 15:57:12 -0800

    Displays icons side-by-side

---
+++ odd-one-out/odd-one-out.py
@@ -0,0 +1,79 @@
+from assets import *
+import random
+
+categories = {
+  'shape': [],
+  'filled': [],
+  'circled': []
+}
+
+bool = random.choice([True, False])
+odd = random.choice(list(categories))
+
+for key, value in categories.items():
+  if key == odd:
+    categories[key].append(bool)
+    for i in range(3):
+      categories[key].append(not bool)
+  else:
+    for i in range(2):
+      categories[key].append(bool)
+      categories[key].append(not bool)
+
+class Icon:
+  def __init__(self, shape, filled, circled, unique):
+    index = 0
+    if circled == True:
+      self.circled = 'Circled'
+    else:
+      self.circled = ''
+      index += 1
+    if filled == True:
+      self.filled = 'Filled'
+      index += 2
+    else:
+      self.filled = 'Empty'
+    if shape == False:
+      self.shape = 'Triangle'
+      self.icon = triangle[index]
+    else:
+      self.shape = 'Square'
+      self.icon = square[index]
+    self.unique = unique
+  def __repr__(self):
+    return self.icon
+
+icons = [0, 1, 2, 3]
+length = 4
+
+for i in range(len(icons)):
+  unique = False
+
+  index = int(random.random() * length)
+  shape = categories['shape'][index]
+  del categories['shape'][index]
+
+  index = int(random.random() * length)
+  filled = categories['filled'][index]
+  del categories['filled'][index]
+
+  index = int(random.random() * length)
+  circled = categories['circled'][index]
+  del categories['circled'][index]
+
+  if odd == 'shape' and shape == bool:
+    unique = True
+  elif odd == 'filled' and filled == bool:
+    unique = True
+  elif odd == 'circled' and circled == bool:
+    unique = True
+
+  icons[i] = Icon(shape, filled, circled, unique)
+  length -= 1
+
+lines = [icons[i].icon.splitlines() for i in range(4)]
+for l in zip(*lines):
+  print(''.join(l))
+
+# for icon in icons:
+#   print(icon)
