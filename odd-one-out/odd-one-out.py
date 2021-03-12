9f9993effa46b1f653e661575b3e04a10002992e
Cyhyraeth <cyhyraethz@gmail.com>
2021-03-11 16:34:59 -0800

    Add replay functionality

--- odd-one-out/odd-one-out.py
+++ odd-one-out/odd-one-out.py
@@ -1,95 +1,102 @@
 from assets import *
 import random
 
-categories = {
-  'shape': [],
-  'filled': [],
-  'circled': []
-}
+play_again = True
 
-bool = random.choice([True, False])
-odd = random.choice(list(categories))
+while play_again:
+  categories = {
+    'shape': [],
+    'filled': [],
+    'circled': []
+  }
 
-for key, value in categories.items():
-  if key == odd:
-    categories[key].append(bool)
-    for i in range(3):
-      categories[key].append(not bool)
-  else:
-    for i in range(2):
+  bool = random.choice([True, False])
+  odd = random.choice(list(categories))
+
+  for key, value in categories.items():
+    if key == odd:
       categories[key].append(bool)
-      categories[key].append(not bool)
-
-class Icon:
-  def __init__(self, shape, filled, circled, unique):
-    index = 0
-    if circled == True:
-      self.circled = 'Circled'
+      for i in range(3):
+        categories[key].append(not bool)
     else:
-      self.circled = ''
-      index += 1
-    if filled == True:
-      self.filled = 'Filled'
-      index += 2
-    else:
-      self.filled = 'Empty'
-    if shape == False:
-      self.shape = 'Triangle'
-      self.icon = triangle[index]
-    else:
-      self.shape = 'Square'
-      self.icon = square[index]
-    self.unique = unique
-  def __repr__(self):
-    return self.icon
+      for i in range(2):
+        categories[key].append(bool)
+        categories[key].append(not bool)
 
-icons = [0, 1, 2, 3]
-length = 4
+  class Icon:
+    def __init__(self, shape, filled, circled, unique):
+      index = 0
+      if circled == True:
+        self.circled = 'Circled'
+      else:
+        self.circled = ''
+        index += 1
+      if filled == True:
+        self.filled = 'Filled'
+        index += 2
+      else:
+        self.filled = 'Empty'
+      if shape == False:
+        self.shape = 'Triangle'
+        self.icon = triangle[index]
+      else:
+        self.shape = 'Square'
+        self.icon = square[index]
+      self.unique = unique
+    def __repr__(self):
+      return self.icon
 
-for i in range(len(icons)):
-  unique = False
+  icons = [0, 1, 2, 3]
+  length = 4
 
-  index = int(random.random() * length)
-  shape = categories['shape'][index]
-  del categories['shape'][index]
+  for i in range(len(icons)):
+    unique = False
 
-  index = int(random.random() * length)
-  filled = categories['filled'][index]
-  del categories['filled'][index]
+    index = int(random.random() * length)
+    shape = categories['shape'][index]
+    del categories['shape'][index]
 
-  index = int(random.random() * length)
-  circled = categories['circled'][index]
-  del categories['circled'][index]
+    index = int(random.random() * length)
+    filled = categories['filled'][index]
+    del categories['filled'][index]
 
-  if odd == 'shape' and shape == bool:
-    unique = True
-  elif odd == 'filled' and filled == bool:
-    unique = True
-  elif odd == 'circled' and circled == bool:
-    unique = True
+    index = int(random.random() * length)
+    circled = categories['circled'][index]
+    del categories['circled'][index]
 
-  icons[i] = Icon(shape, filled, circled, unique)
-  length -= 1
+    if odd == 'shape' and shape == bool:
+      unique = True
+    elif odd == 'filled' and filled == bool:
+      unique = True
+    elif odd == 'circled' and circled == bool:
+      unique = True
+
+    icons[i] = Icon(shape, filled, circled, unique)
+    length -= 1
 
 
-guess = 0
-count = 0
+  guess = 0
+  count = 0
 
-while guess < 1 or guess > 4:
-  if count > 0:
-    print('Error: please enter a valid number between 1 and 4')
-  lines = [icons[i].icon.splitlines() for i in range(4)]
-  for l in zip(*lines):
-    print(''.join(l))
-  print('\n' + ' '*14 + '(1)' + ' '*26 + '(2)' + ' '*26 + '(3)' + ' '*26 + '(4)')
-  try:
-    guess = int(input('\nYour guess: '))
-  except:
-    pass
-  finally:
-    count += 1
+  while guess < 1 or guess > 4:
+    if count > 0:
+      print('Error: please enter a valid number between 1 and 4')
+    lines = [icons[i].icon.splitlines() for i in range(4)]
+    for l in zip(*lines):
+      print(''.join(l))
+    print('\n' + ' '*14 + '(1)' + ' '*26 + '(2)' + ' '*26 + '(3)' + ' '*26 + '(4)')
+    try:
+      guess = int(input('\nYour guess: '))
+    except:
+      pass
+    finally:
+      count += 1
 
-if icons[guess - 1].unique:
-  print('You win!')
-else:
-  print('You lose!')
+  if icons[guess - 1].unique:
+    result = 'win'
+  else:
+    result = 'lose'
+  replay = input(f'You {result}! Play again? (Y/n): ').lower()
+  if len(replay) > 0:
+    if replay[0] == 'n' or replay[0] == 'q' or replay == 'exit':
+      play_again = False
